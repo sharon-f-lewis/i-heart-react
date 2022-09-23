c5ba236f1faf6b0631c7b1c0205f0e971cf5d014
Sharon Lewis <sflewis827@yahoo.com>
2022-09-23 13:27:00 -0400

    Heart Map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import HeartLogo from './heart.svg';
+import Header from './components/Header';
+import HeartsList from './components/HeartsList';
 import './App.css';
-
-const message = 'cool cud';
-
-const Heart = (props) => {
-  return (
-    <div className='heart'>
-      <img className='heart-img' src={HeartLogo} alt='heart' />
-      <p className='heart-message'>{props.msg}</p>
-    </div>
-  )
-};
-
-const Header = () => {
-  return <div className='App-header'>I ♥ React</div>
-};
 
 function App() {
   return (
     <>
       <Header />
-      <Heart msg={message} />
+      <HeartsList />
     </>
   )
 }
